eb771f85448c6decc86809ffaa0d1519dcdfcd99
Francisco Correia28 <francisco.correia1804@gmail.com>
2020-08-11 12:38:42 +0100

    documenting my code

--- server.js
+++ server.js
@@ -20,12 +20,14 @@ const app = express();
 
 app.use(bodyParser.json());
 
+/* Apresentando usuários*/
 app.get('/', (req, res) => {
     (database.users)
         ? res.status(200).json(database.users)
         : res.status(400).json("There's no users to display")
 });
 
+/* Registrando usuários */
 app.post('/register', (req, res) => {
     const { name, email, password } = req.body;
     try {
@@ -44,6 +46,7 @@ app.post('/register', (req, res) => {
 
 });
 
+/* Rota para o login dos usuários */
 app.post('/signin', (req, res) => {
     const { email, password } = req.body;
     if (email === database.users[0].email && JSON.parse(password) === database.users[0].password) {
@@ -53,6 +56,7 @@ app.post('/signin', (req, res) => {
     }
 });
 
+/* buscando o perfil de um usuário pelo id */
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
     const dados = database.users.filter(user => user.id === JSON.parse(id));
@@ -61,8 +65,10 @@ app.get('/profile/:id', (req, res) => {
         : res.status(400).json("User not found")
 });
 
+/* Atualizando as entradas de um usuário, isso será necessário quando se conectar ao frontend devido a api de inteligência artificial */
 app.put('/image', (req, res) => {
-    const dados = database.users.filter(user => user.id === req.params.id);
+    const { id } = req.body;
+    const dados = database.users.filter(user => user.id === JSON.parse(id));
     (dados.length == 1)
         ? res.status(200).json(++dados[0].entries)
         : res.json("Error when trying to update the entries");
