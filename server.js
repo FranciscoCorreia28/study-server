c127d5fd563e0229fa97f0bea980c8f1090c6871
Francisco Correia28 <francisco.correia1804@gmail.com>
2020-08-11 12:09:06 +0100

    few changes

--- server.js
+++ server.js
@@ -24,4 +24,16 @@ app.get('/', (req, res) => {
         : res.status(400).json("There's no users to display")
 });
 
+app.post('/register', (req, res) => {
+    database.users.push({
+        id: 124,
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        entries: 0,
+        joined: new Date()
+    });
+    res.json("new user");
+});
+
 app.listen(3000);
