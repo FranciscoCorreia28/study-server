cd16dc3abbc2b41af31a9c2e7ef35f49fa69d989
Francisco Correia28 <francisco.correia1804@gmail.com>
2020-08-11 12:02:48 +0100

    the first endpoint

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { json } = require('express');
+const database = {
+    users: [
+        {
+            id: 123,
+            name: "Francisco Correia",
+            email: "francisco@gmail.com",
+            password: 123,
+            entries: 0,
+            joined: new Date() // nota: a data não virá formata.
+        }
+    ]
+}
 
+const app = express();
 
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    (database.users)
+        ? res.status(200).json(database.users)
+        : res.status(400).json("There's no users to display")
+});
 
 app.listen(3000);
